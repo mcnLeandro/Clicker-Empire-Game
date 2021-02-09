1de971e658f63260be8804eeddc5ad22a407718f
Leandro <leandro83g@icloud.com>
2021-02-10 00:51:40 +0900

    Item,Typeのインスタンス作成、初期化。Imgクラスの追加。DBにwhereメソッドの追加

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ class DB {
         this.table[key] = {};
     }
     static add(obj) {
-
         let i = this.idCounter++;
         let className = this.name;
 
@@ -23,18 +22,29 @@ class DB {
 
         obj.id = i;
         this.table[className][i] = obj;
-
     }
     static find(id) {
-
         let className = this.name;
         return this.table[className][id]
+    }
+    static where(key1,value1, key2, value2){
+        let className = this.name
+        let models = this.table[className]
 
+        let arr = []
+        Object.keys(models).forEach(id =>{
+            let model = models[id]
+            if(model[key1] == value1 && model[key2] == value2) arr.push(model);
+        })
+
+        return arr;
     }
     static all() {
         let className = this.name;
         return this.table[className]
     }
+
+    
     toKeyFormat(string) {
         let newString = string[0];
         let uppers = string.substring(1, string.length - 1).match(/[A-Z]/g)
@@ -46,7 +56,6 @@ class DB {
             newString += string.substring(0, upperIndex)
             newString += "_" + string[upperIndex]
             string = string.substring(upperIndex+1,string.length)
-            console.log(string)
         });
     
         return (newString + string).toLowerCase();
@@ -60,6 +69,8 @@ class DB {
         this[`${modelName}s`] = () => {
 
             let newObj = {}
+
+            if(!DB.table[model.name]) DB.initializeTable(model.name)
 
             Object.keys(DB.table[model.name]).forEach(record=> {
                 let modelObjs = DB.table[model.name]
@@ -80,14 +91,15 @@ class DB {
     //=========================================================
 
     static getStringOf(data){
+        if (data == null ) data = "null"
+
         if (typeof (data) == "string")   data = data
         if (typeof (data) == "number")   data = String(data);
-        if (typeof (data) == "function") data = String(data())
-        if (typeof (data) == "object")   data = String(Object.keys(data))
+        if (typeof (data) == "function") data = "f()"
+        if (typeof (data) == "object")   data = "{Object}"
 
         return data;
     }
-    //データがひとつもない場合はエラーになるので修正が必要
     static getMaxColumnLengthArr(obj) {
         let arr = []
 
@@ -144,7 +156,6 @@ class DB {
 
 
     }
-    //これも一つもデータがない状態だとエラーになる
     static getColumnRecordString(model,lenArr){
         let string = "";
         let line = "|";
@@ -212,6 +223,7 @@ class DB {
 //=================================================
 //Model classes
 //=================================================
+
 class User extends DB {
     constructor(id, name, age, totalMoney, earningPerDay){
         super(id)
@@ -231,6 +243,7 @@ class Time extends DB {
         super(id)
 
         this.user_id  = user_id
+
         this.day = day
 
         super.belongsTo(User)
@@ -242,6 +255,7 @@ class BoughtItem extends DB {
 
         this.user_id = user_id
         this.item_id = item_id
+
         this.amount  = amount
 
         super.belongsTo(User)
@@ -249,13 +263,15 @@ class BoughtItem extends DB {
     }
 }
 class Product extends DB {
-    constructor(id,user_id, item_id, amount, earning, makerAmount){
+    constructor(id,user_id, item_id, name, amount, earning, makerAmount){
         super(id)
 
         this.user_id = user_id
         this.item_id = item_id
-        this.amount  = amount
-        this.earning   = earning
+
+        this.name        = name
+        this.amount      = amount
+        this.earning     = earning
         this.makerAmount = makerAmount
 
         super.belongsTo(User)
@@ -263,16 +279,19 @@ class Product extends DB {
     }
 }
 class Item extends DB {
-    constructor(id, type_id, name, introduction, imageUrl, stock, price){
+    constructor(id,type_id, img_id, name, introduction, stock, price){
         super(id)
 
-        this.type_id = type_id
+        this.type_id  = type_id
+        this.img_id = img_id
+
         this.name = name
         this.introduction = introduction
-        this.imageUrl = imageUrl
         this.stock = stock
         this.price = price
 
+        super.belongsTo(Type)
+        super.belongsTo(Img)
         super.hasmany(BoughtItem)
         super.hasmany(Product)
     }
@@ -282,9 +301,85 @@ class Type extends DB {
         super(id)
 
         this.name = name
-        this.effection = effection
-
+        this.effection = () => effection
+        console.log(typeof( () => effection))
         super.hasmany(Item)
+    }
+}
+class Img extends DB {
+    constructor(id ,name, url){
+        super(id)
+        this.name = name
+        this.url  = url
+    }
+}
+//==============================================
+//modelのヘルプ関数などを管理する。
+//基本的にはtypeのeffection関数を管理することになる。
+//ちょっとダサいかもしれないけど今回はこれで行く
+//==============================================
+
+class ModelHelper{
+    static BoughtItemAddtionProcess(user_id,item_id){
+
+        if(BoughtItem.where("user_id",user_id, "item_id",item_id).length == 0){
+            let newBoughtItem = new BoughtItem(null, user_id, item_id, 0)
+            BoughtItem.add(newBoughtItem);
+        }
+        else{
+            BoughtItem.where("user_id",user_id, "item_id",item_id)[0].amount += 1;
+        }
+
+    }
+    static productTypeEffection(user_id,item_id, productEarning){
+
+        ModelHelper.BoughtItemAddtionProcess(user_id,item_id);
+        
+        let itemName = Item.find(item_id).name
+        let newProduct = new Product(null, user_id, item_id, itemName, 0 ,productEarning, 0);
+        Product.add(newProduct);
+
+    }
+    static abilityTypeEffection(user_id, item_id, additonalPrice){
+
+        ModelHelper.BoughtItemAddtionProcess(user_id,item_id);
+
+        let product = Product.where("user_id",user_id,"item_id",item_id)[0];
+        product.earningPerDay += additonalPrice;
+
+    }
+    static manpowerTypeEffection(user_id,item_id){
+
+        ModelHelper.BoughtItemAddtionProcess(user_id,item_id);
+
+        let product = Product.where("user_id",user_id,"item_id",item_id)[0];
+        product.makerAmount += 1;
+
+    }
+    //3000 *= 10 roop問題あり
+    static investimentTypeEffection(user_id, item_id, returnPercentage, itemPriceChange){
+
+        ModelHelper.BoughtItemAddtionProcess(user_id,item_id);
+
+        let boughtItem = BoughtItem.where("user_id",user_id,"item_id",item_id)[0];
+        let itemPrice = boughtItem.item().price;
+        let itemAmount = boughtItem.amount;
+
+        let additionalReturn = Math.round(itemPrice * (itemAmount) * (returnPercentage/100)) - Math.round(itemPrice * (itemAmount-1) * (returnPercentage/100));
+
+        let user = User.find(user_id);
+        user.earningPerDay += additionalReturn;
+
+        
+    }
+    static realEstateTypeEffection(user_id, item_id, additionalReturn){
+
+        ModelHelper.BoughtItemAddtionProcess(user_id,item_id);
+
+        let user = User.find(user_id)
+
+        user.earningPerDay += additionalReturn;
+
     }
 }
 
@@ -292,3 +387,38 @@ class Type extends DB {
 //DBの初期化 (initilize DB )
 //==============================================
 
+[
+    new Img(null,"Hamburger","https://cdn.pixabay.com/photo/2012/04/14/15/37/cheeseburger-34315_1280.png")
+].forEach(img => Img.add(img));
+
+
+[
+    new Item(null, 1 , 1 , "Hamburger"               , "Release Hamburger"                                , 1   , 30000         ),
+    new Item(null, 1 , 1 , "Lemonade"                , "Release Lemonade"                                 , 1   , 60000         ),
+    new Item(null, 1 , 1 , "Ice Cream"               , "Release Ice Cream"                                , 1   , 90000         ),
+    new Item(null, 2 , 1 , "Flip machine"            , "Increase earning from making Hamburger by 25 yen" , 500 , 15000         ),
+    new Item(null, 2 , 1 , "Lemonade Stand"          , "Increase earning from making Lemonade by 50 yen"  , 1000, 30000         ),
+    new Item(null, 2 , 1 , "Ice Cream Truck"         , "Increase earning from making Ice Cream by 150 yen", 500 , 100000        ),
+    new Item(null, 3 , 1 , "Hamburger Coworker"      , "Employ worker for making Hamburger"               , 10  , 3000          ),
+    new Item(null, 3 , 1 , "Lemonade Coworker"       , "Employ worker for making Lemonade"                , 10  , 3000          ),
+    new Item(null, 3 , 1 , "Ice Cream Coworker"      , "Employ worker for making Ice Cream"               , 10  , 3000          ),
+    new Item(null, 4 , 1 , "ETF Stock"               , "Get 0.1% of total price of  this item you bought" , null, 300000        ),
+    new Item(null, 4 , 1 , "ETF Bonds"               , "Get 0.07% of total price of  this item you bought", null, 300000        ),
+    new Item(null, 5 , 1 , "House"                   , "Get 32000 yen per day"                            , 100 , 20000000      ),
+    new Item(null, 5 , 1 , "TownHouse"               , "Get 64000 yen per day"                            , 100 , 40000000      ),
+    new Item(null, 5 , 1 , "Mansion"                 , "Get 500000 yen per day"                           , 20  , 250000000     ),
+    new Item(null, 5 , 1 , "Industrial Space"        , "Get 2200000 yen per day"                          , 10  , 1000000000    ),
+    new Item(null, 5 , 1 , "Hotel Skyscraper"        , "Get 25000000 yen per day"                         , 5   , 10000000000   ),
+    new Item(null, 5 , 1 , "Bullet-Speed Sky Railway", "Get 30000000000 yen per day"                      , 1   , 10000000000000)
+].forEach(item => Item.add(item));
+
+
+[
+    new Type(null, "Product"     , ModelHelper.productTypeEffection    ),
+    new Type(null, "Ability"     , ModelHelper.abilityTypeEffection    ),
+    new Type(null, "Manpower"    , ModelHelper.manpowerTypeEffection   ),
+    new Type(null, "Investiment" , ModelHelper.investimentTypeEffection),
+    new Type(null, "Real estate" , ModelHelper.realEstateTypeEffection )
+].forEach(type => Type.add(type))
+
+console.log(DB.showDB())
