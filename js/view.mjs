c7f37a5b355361fdf90c956f2375f6de5e7683b6
Leandro <leandro83g@icloud.com>
2021-02-19 14:41:46 +0900

    Viewのリファクタリング

--- js/view.mjs
+++ js/view.mjs
@@ -6,55 +6,50 @@ import { Slider } from './slider.mjs'
 export class View { 
     
     static top(){
-        let innerMain =`
-            <div class="d-flex justify-content-center h-90vh">
-                <div class="col-6 mt-auto mb-auto">
-                    <button id="newGameBtn" class="btn btn-primary w-100 m-3">NewGame</button>
-                    <button id="loginBtn" class="btn btn-primary w-100 m-3">Log in</button>
+
+        document.getElementById("body").innerHTML = ViewTemplate.base(
+            `
+                <div class="d-flex justify-content-center h-90vh">
+                    <div class="col-6 mt-auto mb-auto">
+                        <button id="newGameBtn" class="btn btn-primary w-100 m-3">NewGame</button>
+                        <button id="loginBtn" class="btn btn-primary w-100 m-3">Log in</button>
+                    </div>
                 </div>
-            </div>
-        `
-
-
-        document.getElementById("body").innerHTML = ViewTemplate.base(innerMain)
-
+            `
+        )
 
         Render.navLinks()
         Render.clickToSignUp("newGameBtn")
         Render.clickToLogin("loginBtn")
+
     }
     static signUp(){
-        let innnerMain = `
-            <div class="d-flex justify-content-center h-90vh">
-                <div class="col-6 mt-auto mb-auto">
-                    <h2>What's your Name ?</h2>
-                    <input id="nameInput" type="text" class="form-control m-3">
-                    <button id="startGameBtn" class="btn btn-primary w-100 m-3">Start Game</button>
+
+
+        document.getElementById("body").innerHTML = ViewTemplate.base(
+            `
+                <div class="d-flex justify-content-center h-90vh">
+                    <div class="col-6 mt-auto mb-auto">
+                        <h2>What's your Name ?</h2>
+                        <input id="nameInput" type="text" class="form-control m-3">
+                        <button id="startGameBtn" class="btn btn-primary w-100 m-3">Start Game</button>
+                    </div>
                 </div>
-            </div>
-        `
-
-
-        document.getElementById("body").innerHTML = ViewTemplate.base(innnerMain)
-
+            `
+        )
 
         Render.navLinks()
         Render.clickToRegistration("startGameBtn")
+
+
     }
     static login(){
         //ログイン画面
     }
-    static app(){
-
-        document.getElementById("body").innerHTML = ViewTemplate.base(ViewTemplate.frames())
-        View.userInfo(User.currentUser())
-        View.itemIndex()
-        View.productInfoWithSlider()
-
-        Render.navLinks()
-    }
     static userInfo(user){
-        let userInfo =  `
+
+
+        document.getElementById("userInfoFrame").innerHTML =  `
             <div class="row mx-1 px-2 justify-content-center bg-heavy-gray rounded">
                 <div class="col-xl col-lg-4 col-10 m-3 p-3 bg-light-gray rounded">${user.name}</div>
                 <div class="col-xl col-lg-4 col-10 m-3 p-3 bg-light-gray rounded">${user.age.toLocaleString()} yrs old</div>
@@ -62,17 +57,16 @@ export class View {
                 <div class="col-xl col-lg-4 col-10 m-3 p-3 bg-light-gray rounded">${user.totalMoney.toLocaleString()} yen</div>
             </div>
         `
-        document.getElementById("userInfoFrame").innerHTML = userInfo;
-
 
     }
     static productInfoWithSlider(){
-        let productData = ""
 
+        
+        let productData = ""
         User.currentUser().users_products().forEach(product => productData += ViewTemplate.productInfo(product)) 
 
 
-        let productInfoWithSlider = `
+        document.getElementById("productInfoFrame").innerHTML = `
         <div id="slideFrame" class="row  mx-5 justify-content-center bg-heavy-gray rounded ">
             <div id="productSliderShowFrame" class="row flex-nowrap overflow-hiddens pl-lg-5 pl-3">
                 <div id="productSliderMain" class="main full-width">
@@ -95,7 +89,6 @@ export class View {
             </div>
         </div>
         `
-        document.getElementById("productInfoFrame").innerHTML = productInfoWithSlider;
 
         new Slider(
             "sliderProduct",
@@ -105,20 +98,22 @@ export class View {
             "productSliderLeftBtn",
             "productSliderRightBtn"
         ).set()
+
+
     }
     static itemIndex(){
+
+
         let items = ""
         Item.all().forEach(item => items += ViewTemplate.item(item))
 
-        let itemIndex =  `
+        document.getElementById("itemInfoFrame").innerHTML =  `
             <div class="row mx-1 px-2 justify-content-center bg-heavy-gray rounded">
 
                 ${items}
                 
             </div>
         `
-
-        document.getElementById("itemInfoFrame").innerHTML = itemIndex;
 
         Item.all().forEach(item => {
             if(document.querySelector(`#item${item.id} #itemShowBtn`)){
@@ -129,10 +124,13 @@ export class View {
             
         })
 
+
     }
     //購入できる数はユーザーがいくつそのitemを持っているかで変更していく必要がある。
     //その辺の曖昧な部分をまだ考える必要がある。
     static itemShow(item){
+
+
         let inputControl = () =>{
             let input = document.querySelector("#itemQuantityInput");
             let value = input.value;
@@ -161,39 +159,37 @@ export class View {
         }
 
 
-        let itemShow = `
-        <div class="row mx-1 px-2 justify-content-center bg-heavy-gray rounded">
+        document.getElementById("itemInfoFrame").innerHTML = `
+            <div class="row mx-1 px-2 justify-content-center bg-heavy-gray rounded">
 
-            <section class="mt-2 p-2 rounded col-12" >
-                <div class="m-2 p-5 shadow bg-light-gray rounded">
-                    <div>
-                        <i id="goBackIcon" class="fas fa-arrow-circle-left fa-3x click-object"></i>
-                    </div>
-                    <div class="col-12 col-lg-3 ml-lg-auto float-lg-right">
-                        <img src="${item.img().url}" alt="" width="100%" height="" class="rounded">
-                    </div>
-                    <div class="p-2 col-7 ">
-                        <h2>${item.name}</h2>
-                        <p>Max Purchases : ${item.stock != null ? item.stock.toLocaleString() : "∞"}</p>
-                        <p>Price : ${item.price.toLocaleString()}</p>
-                        <h3>Effection</h3>
-                        <p>${item.introduction()}</p>
-                    </div>
-                    <div class="mt-2 d-flex justify-content-end ">
-                        <div class="col-12 col-md-5 p-0 text-right">
-                            <p>How many would you like to purchse?</p>
-                            <input id="itemQuantityInput" type="number" class="form-control ml-auto my-2" min="0" max="${item.stock}">
-                            <p id="totalPriceP" class="border-bottom">Total : ${calculateTotalPrice()}</p>
-                            <button id="purchaseBtn" class="btn btn-info w-100"> Purchase</button>
+                <section class="mt-2 p-2 rounded col-12" >
+                    <div class="m-2 p-5 shadow bg-light-gray rounded">
+                        <div>
+                            <i id="goBackIcon" class="fas fa-arrow-circle-left fa-3x click-object"></i>
+                        </div>
+                        <div class="col-12 col-lg-3 ml-lg-auto float-lg-right">
+                            <img src="${item.img().url}" alt="" width="100%" height="" class="rounded">
+                        </div>
+                        <div class="p-2 col-7 ">
+                            <h2>${item.name}</h2>
+                            <p>Max Purchases : ${item.stock != null ? item.stock.toLocaleString() : "∞"}</p>
+                            <p>Price : ${item.price.toLocaleString()}</p>
+                            <h3>Effection</h3>
+                            <p>${item.introduction()}</p>
+                        </div>
+                        <div class="mt-2 d-flex justify-content-end ">
+                            <div class="col-12 col-md-5 p-0 text-right">
+                                <p>How many would you like to purchse?</p>
+                                <input id="itemQuantityInput" type="number" class="form-control ml-auto my-2" min="0" max="${item.stock}">
+                                <p id="totalPriceP" class="border-bottom">Total : ${calculateTotalPrice()}</p>
+                                <button id="purchaseBtn" class="btn btn-info w-100"> Purchase</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
-            
-        </div>
+                </section>
+                
+            </div>
         `
-
-        document.getElementById("itemInfoFrame").innerHTML = itemShow;
 
 
         Render.clickToLoadItemIndex("goBackIcon")
@@ -209,11 +205,26 @@ export class View {
 
 
     }
+    static app(){
+
+
+        document.getElementById("body").innerHTML = ViewTemplate.base(ViewTemplate.frames())
+        View.userInfo(User.currentUser())
+        View.itemIndex()
+        View.productInfoWithSlider()
+
+        Render.navLinks()
+
+
+    }
 
 }
 
 export class ViewTemplate {
+    
     static base(view){
+
+
         return `
             <header class="p-2 d-flex justify-content-between bg-dark">
                 <h2 class="white text-3vw" >Clicker Empire Game</h2>
@@ -236,8 +247,12 @@ export class ViewTemplate {
                 </div>
             </footer>
         `
+
+
     }
     static nav(){
+
+
         return `
         <ul class="nav">
             <li class="nav-item">
@@ -259,8 +274,12 @@ export class ViewTemplate {
             </li>
         </ul>
         `
+
+
     }
     static frames(userInfo, productInfo, itemInfo){
+
+
         return `
             <div  class="col-lg-7 col-12 bg-light-gray p-3 float-lg-right user-info-section">
                 <!-- 
@@ -293,30 +312,46 @@ export class ViewTemplate {
                 </div>
             </div>
         `
+
+
     }
     
     static productDescription(usersProduct){
+
+
         return `
             <div class="text-center p-2 white">${usersProduct.amount} ${usersProduct.name.toLowerCase()}s</div>
             <div class="text-center p-2 white">${usersProduct.earning} yen per day </div>
         `
+
+
     }
     static productMakers(usersProduct){
+
+
         let maker = `<i class="fas fa-user fa-2x white up_down"></i>` + `\n`
         let makers = ``;
 
         for(let i = 1 ; i <= usersProduct.makerAmount; i++) makers += maker;
 
         return makers;
+
+
     }
     static productImg(usersProduct){
+
+
         return `
             <div class="d-flex justify-content-center">
                 <img src="${usersProduct.img().url}" alt="" width="70%">
             </div>
         `
+
+
     }
     static productInfo(usersProduct){
+
+
         return `
             <div class="sliderProduct">
                 <div id="productDescription" class="col-11 my-3 bg-light-gray rounded">
@@ -330,9 +365,12 @@ export class ViewTemplate {
                 </div>
             </div>
         `
+
+
     }
-    
     static item(item){
+
+
         let owning = ()=>{
             if(UsersItem.where("user_id",User.currentUser().id,"item_id",item.id).length == 0) return 0
             else return UsersItem.where("user_id",User.currentUser().id,"item_id",item.id)[0].amount
@@ -355,7 +393,7 @@ export class ViewTemplate {
                 </div>
             </section>
         `
+
+
     }
-    
-    
 }
